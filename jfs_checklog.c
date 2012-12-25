88d68ed5635e6dc9860d4b42b60a2c1e8cf6e1e9
Sachin Pande <sazap10@gmail.com>
2012-12-25 21:21:17 +0000

    cast correctly in while loop

--- jfs_checklog.c
+++ jfs_checklog.c
@@ -23,7 +23,7 @@ void checklog(jfs_t *jfs)
 		jfs_read_block(jfs,block,logfile_i_node.blockptrs[0]);
 		magicnum = (unsigned int)block;
 		while(*magicnum !=0x89abcdef){
-			magicnum = (unsigned int)(block + sizeof(unsigned int));
+			magicnum = (unsigned int*)(block + sizeof(unsigned int));
 			bytes_done+=sizeof(unsigned int);
 			//do stuff with block
 			//printf("%s",block);
