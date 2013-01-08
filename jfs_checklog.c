3a8bf17b83354e3553ce2775e396159c32a7e55f
Sachin Pande <sazap10@gmail.com>
2013-01-08 10:54:11 +0000

    all comments added and also checking checksum

--- jfs_checklog.c
+++ jfs_checklog.c
@@ -20,30 +20,41 @@ void write_Data_To_Disk(jfs_t *jfs,int * blocks,struct inode logfile_i_node){
 		i++;
 	}
 }
-
+/*This is the main function for jfs_checklog command*/
 void checklog(jfs_t *jfs)
 {
-    int root_inode,logfile_inode;
+    int root_inode,logfile_inode,i = 0,checksum =0;
 	struct inode logfile_i_node;
 	char block[BLOCKSIZE];
 	struct commit_block *commitblock;
-	int i = 0;
-	
+	//get the inode number of the root directory
     root_inode = find_root_directory(jfs);
+	//use the findfile_recursive to find the inode number of the log file
     logfile_inode = findfile_recursive(jfs, ".log", root_inode, DT_FILE);
+	//logfile is not found on the system so throw an error
     if (logfile_inode < 0) {
 		fprintf(stderr, "Missing logfile!\n");
-    }else{
+    }
+	//otherwise continue on with the command
+	else{
+		//get the actual inode structure of the log file using the inode number found before
 		get_inode(jfs, logfile_inode, &logfile_i_node);
+		//loop through the block pointer in the inode
 		while(logfile_i_node.blockptrs[i]){
+			//add the block number to the checksum
+			checksum+=blockptrs[i];
+			//read in the block using the number from the block pointers array
 			jfs_read_block(jfs,block,logfile_i_node.blockptrs[i]);
+			//cast this block into a commit block
 			commitblock = (struct commit_block *)block;
+			//check if its the commit block by checking against the magic number
 			if(commitblock->magicnum ==0x89abcdef){
-				//printf("commit block found\n");
-				if(commitblock->uncommitted){
+				/*if the commit block has not been committed to disk and the checksum is equal to the one on the commit block
+				*then call the write_Data_To_Disk function to write the data to disk
+				*/
+				if(commitblock->uncommitted && commitblock->sum == checksum){
 					write_Data_To_Disk(jfs,commitblock->blocknums,logfile_i_node);
 				}
-				memset(block,0,BLOCKSIZE);
 				break;
 			}
 			i++;
