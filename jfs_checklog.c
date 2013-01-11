e83c837b1d4cc4636260315cafc497b78bc7cb1a
Sachin Pande <sazap10@gmail.com>
2013-01-11 18:57:17 +0000

    created submission zip

--- jfs_checklog.c
+++ jfs_checklog.c
@@ -52,7 +52,7 @@ void checklog(jfs_t *jfs){
 				if(commitblock->uncommitted){
 					write_Data_To_Disk(jfs,commitblock->blocknums,logfile_i_node);
 				}
-				
+				//overwrite the commit block marking it as committed and cancelling it
 				if (i < INODE_BLOCK_PTRS) {
 					int j;
 					for (j = 0; j < INODE_BLOCK_PTRS; j++)
